2cfc07dbe935c334ecc3d084c842b05ed1382fa5
bloges56 <brady.drew.logan@gmail.com>
2021-06-25 10:32:01 -0500

    program now outputs to files and added user experience

--- Program.cs
+++ Program.cs
@@ -6,9 +6,8 @@ namespace PolyalphaCipher
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            string plainText = System.IO.File.ReadAllText(args[0]).ToLower();
 
             Dictionary<char, char> M2 = new Dictionary<char, char>();
             M2.Add('a', 'd');
@@ -38,13 +37,34 @@ namespace PolyalphaCipher
             M2.Add('y', 'z');
             M2.Add('z', 'n');
 
-            string cipherText = Encrypt(plainText, M2, 1);
+            Console.WriteLine("1)Encrypt\n2)Decrypt");
 
-            Console.WriteLine(cipherText);
+            int choice = Int32.Parse(Console.ReadLine());
+            string plainText, cipherText, file;
+            if(choice == 1)
+            {
+                Console.WriteLine("Enter the path of the text file you wish to encrypt:");
+                file = Console.ReadLine();
+                plainText = System.IO.File.ReadAllText(file).ToLower();
+                cipherText = Encrypt(plainText, M2, 1);
+                Console.WriteLine("Ciphertext: " + cipherText);
+                System.IO.File.WriteAllTextAsync("cipherTextOut.txt", cipherText);
+            }
 
-            plainText = Decrypt(cipherText, M2, 1);
+            else if (choice == 2)
+            {
+                Console.WriteLine("Enter the path of the text file you wish to decrypt:");
+                file = Console.ReadLine();
+                cipherText = System.IO.File.ReadAllText(file).ToLower();
+                plainText = Decrypt(cipherText, M2, 1);
+                Console.WriteLine("PlainText: " + plainText);
+                System.IO.File.WriteAllTextAsync("plainTextOut.txt", plainText);
+            }
 
-            Console.WriteLine(plainText);
+           else
+            {
+                Console.WriteLine("Please enter a valid choice.");
+            }
 
 
         }
